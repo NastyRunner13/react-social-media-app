2007ec24cd060fee38faca63446a4bd3b23a87b5
Prince Gupta <princegupta1586@gmail.com>
2023-01-22 18:23:24 +0530

    Add Leftbar

--- src/components/leftBar/Leftbar.jsx
+++ src/components/leftBar/Leftbar.jsx
@@ -4,7 +4,72 @@ export default function Leftbar() {
   return (
     <div className="leftbar">
       <div className="container">
-        <div className="menu"></div>
+        <div className="menu">
+          <div className="user">
+            <img src="" alt="" />
+            <span>Prince Gupta</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Friends</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Marketplace</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Watch</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Memories</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Your Shortcuts</span>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Gaming</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Gallery</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Videos</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Others</span>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Fundraisers</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Tutorials</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Courses</span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/rightBar/Rightbar.jsx
+++ src/components/rightBar/Rightbar.jsx
@@ -1,5 +1,45 @@
 import "./rightbar.scss";
 
 export default function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="" alt="" />
+              <span>Prince Gupta</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Unfollow</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="" alt="" />
+              <span>Prince Gupta</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Unfollow</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="" alt="" />
+              <p>
+                <span>Prince Gupta</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
