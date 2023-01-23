ef767670d57608e2d1b247ad76bed1bb7c8b2571
Prince Gupta <princegupta1586@gmail.com>
2023-01-23 17:27:52 +0530

    Add Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,12 @@
 import "./home.scss";
+import Stories from "../../components/stories/Stories";
+import Posts from "../../components/posts/Posts";
 
 export default function home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Stories />
+      <Posts />
+    </div>
+  );
 }
